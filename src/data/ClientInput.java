48ef66bf718ca36c54bf667b3782f8b56ce07dfa
Thomas <Thomas@Thomas-PC>
2015-03-29 10:42:32 +0200

    Giver ikke fejl hvis der er mere end 6 " i rm20 kommando

--- src/data/ClientInput.java
+++ src/data/ClientInput.java
@@ -36,14 +36,16 @@ public class ClientInput extends Thread {
 		int j = 0;
 		while(i < str.length()) {
 			if(str.charAt(i) == '\"') {
-				indices[j] = i;
-				j++;
+				if(indices[5] == 0) {
+					indices[j] = i;
+					j++;
+				}
 				count++;
 			}
 			i++;
 		}
 		if(count == 6) {
-			//check to see if there one and only one space between the 2-3 and 4-5 quotations marks
+			//check to see if there is one and only one space between the 2nd-3rd and 4th-5th quotations marks
 			if(str.charAt(indices[1]+1)== ' ' && str.charAt(indices[1]+2) == '\"' 
 					&& str.charAt(indices[3]+1) == ' ' && str.charAt(indices[3]+2) == '\"' 
 					&& str.indexOf('\"', indices[5]) == str.lastIndexOf('\"')) {
@@ -73,17 +75,15 @@ public class ClientInput extends Thread {
 			}
 			try{
 				while (!(inline = instream.readLine().toUpperCase()).isEmpty()){
-					 boolean correctmsg = false;
+					boolean correctmsg = false;
 					//System.out.println(inline);
 					//When we get a message with RM20 8 we will reply with a message from the server.
 					if (inline.startsWith("RM20 8")){
 						inline = inline.substring(7, inline.length()).trim();
 						//Validation check
 						if(checkRM20(inline)) {
-
 							correctmsg = true; 
 							System.out.print("Tryk ENTER og derefter dit svar");
-
 							Simulator.setInstruktionsDisplay(inline);
 							Simulator.printmenu();
 							System.out.print("\nBesked modtaget. Se instruktionsdisplay.\nTryk ENTER og derefter dit svar");
@@ -101,7 +101,9 @@ public class ClientInput extends Thread {
 								outstream.writeBytes("RM20 A "+input+ "\r\n");
 							} else outstream.writeBytes("RM20 A "+input+ "\r\n");
 						}
-					}else if(inline.startsWith("P111")){
+					}
+
+					else if(inline.startsWith("P111")){
 						if(inline.length() <= 35){
 							if(inline.charAt(5)== '\"' && inline.charAt(inline.length()-1) == '\"'){
 								Simulator.setInstruktionsDisplay(inline.substring(3, inline.length()-1).trim());
@@ -109,19 +111,22 @@ public class ClientInput extends Thread {
 								outstream.writeBytes("P111 A"+"\r\n");
 								correctmsg = true;
 							}
-//							else{
-//								outstream.writeBytes("S"+"\r\n");
-//							}
+							//							else{
+							//								outstream.writeBytes("S"+"\r\n");
+							//							}
 						}
-//						else{
-//							outstream.writeBytes("S"+"\r\n");	
-//						}	
-					}else if (inline.equals("DW")){
+						//						else{
+						//							outstream.writeBytes("S"+"\r\n");	
+						//						}	
+					}
+
+					else if (inline.equals("DW")){
 						Simulator.setWeightDisplay("");
 						Simulator.printmenu();
 						outstream.writeBytes("DW A"+"\r\n");
 						correctmsg = true; 
 					}
+
 					else if (inline.startsWith("D")){
 						if (inline.equals("D")){
 							Simulator.setWeightDisplay("");
@@ -142,6 +147,7 @@ public class ClientInput extends Thread {
 
 						}						
 					}
+
 					else if (inline.startsWith("T")){
 						if (inline.equals("T")){
 
@@ -152,16 +158,18 @@ public class ClientInput extends Thread {
 								correctmsg = true; 
 							}
 						}
-//						else{
-//							outstream.writeBytes("S" +"\r\n");
-//						}
+						//						else{
+						//							outstream.writeBytes("S" +"\r\n");
+						//						}
 						Simulator.printmenu();
 					}
+
 					else if (inline.equals("S")){
 						Simulator.printmenu();
 						outstream.writeBytes("S S      " + (Simulator.getBrutto()-Simulator.getTara())+ " kg"  +"\r\n");
 						correctmsg = true; 
 					}
+
 					else if (inline.startsWith("B")){ // denne ordre findes ikke på en fysisk vægt
 						if(inline.length() > 5){
 							String temp= inline.substring(2,inline.length()).trim();
@@ -175,6 +183,7 @@ public class ClientInput extends Thread {
 						}
 
 					}
+
 					else if ((inline.equals("Q"))){
 						System.out.println("");
 						System.out.println("Program stoppet Q modtaget paa com port");
@@ -188,7 +197,7 @@ public class ClientInput extends Thread {
 					}
 					if(!correctmsg){
 						outstream.writeBytes("S"+"\r\n");
-				}
+					}
 				}
 			}catch(NullPointerException e1){
 				//when a client terminates his connection i.e closes his computer or connection program
